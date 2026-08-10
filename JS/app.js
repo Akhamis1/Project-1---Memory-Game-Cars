7e0f8b8517bbf5744b8fc2f2a088b4f112a3ee90
Akhamis1 <ammar779@outlook.com>
2026-08-10 19:25:42 +0300

    add CardImage array with the pics

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,23 @@
 /*-------------------------------- Constants --------------------------------*/
+const cardImages =[
+    "../Photos/Car1.png",
+    "../Photos/Car1.png",
 
+    "../Photos/Car2.png",
+    "../Photos/Car2.png",
+
+    "../Photos/Car3.png",
+    "../Photos/Car3.png",
+
+    "../Photos/Car4.png",
+    "../Photos/Car4.png",
+
+    "../Photos/Car5.png",
+    "../Photos/Car5.png",
+
+    "../Photos/Car6.png",
+    "../Photos/Car6.png",
+]
 /*---------------------------- Variables (state) ----------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
