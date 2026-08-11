c1093e75d45111e7b9fb32664e2e25812e472d17
Akhamis1 <ammar779@outlook.com>
2026-08-11 18:20:17 +0300

    add the arrar to store the pictures and the varibles and cashed all element

--- JS/app.js
+++ JS/app.js
@@ -19,10 +19,26 @@ const cardImages =[
     "../Photos/Car6.png",
 ]
 /*---------------------------- Variables (state) ----------------------------*/
+let firstCard
+let SecoundCard
+let score
+let lives
+let duration
+let timer
+
 
 /*------------------------ Cached Element References ------------------------*/
+const messageElement = document.querySelector("#message")
+const cardElement = document.querySelectorAll(".card")
+const scoreElement =document.querySelector("#score")
+const durationElement =document.querySelector("#duration")
+const diffcultyElement =document.querySelector("#difficulty")
+const livesElement =document.querySelector("#lives")
+const restartElement =document.querySelector("#restart-btn")
+const StartElement =document.querySelector("#start-btn")
 
 /*-------------------------------- Functions --------------------------------*/
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 
