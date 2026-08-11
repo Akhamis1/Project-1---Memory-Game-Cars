dd102ab2f6b5c538634bc5cc839cc324beb5ccd9
Akhamis1 <ammar779@outlook.com>
2026-08-11 20:26:16 +0300

    add  initialization and difficulty settings

--- JS/app.js
+++ JS/app.js
@@ -1,31 +1,32 @@
 /*-------------------------------- Constants --------------------------------*/
 const cardImages =[
-    "../Photos/Car1.png",
-    "../Photos/Car1.png",
+"../Photos/Car1.png",
+"../Photos/Car1.png",
 
-    "../Photos/Car2.png",
-    "../Photos/Car2.png",
+"../Photos/Car2.png",
+"../Photos/Car2.png",
 
-    "../Photos/Car3.png",
-    "../Photos/Car3.png",
+"../Photos/Car3.png",
+"../Photos/Car3.png",
 
-    "../Photos/Car4.png",
-    "../Photos/Car4.png",
+"../Photos/Car4.png",
+"../Photos/Car4.png",
 
-    "../Photos/Car5.png",
-    "../Photos/Car5.png",
+"../Photos/Car5.png",
+"../Photos/Car5.png",
 
-    "../Photos/Car6.png",
-    "../Photos/Car6.png",
+"../Photos/Car6.png",
+"../Photos/Car6.png",
 ]
+console.log(cardImages)
 /*---------------------------- Variables (state) ----------------------------*/
-let firstCard
-let SecoundCard
-let score
-let lives
-let duration
-let timer
 
+let firstCard
+let secondCard
+let score
+let duration
+let lives
+let timer
 
 /*------------------------ Cached Element References ------------------------*/
 const messageElement = document.querySelector("#message")
@@ -37,9 +38,41 @@ const livesElement =document.querySelector("#lives")
 const restartElement =document.querySelector("#restart-btn")
 const StartElement =document.querySelector("#start-btn")
 
+
+
+console.log( messageElement,cardElement,scoreElement,durationElement,diffcultyElement,livesElement,restartElement,StartElement)
 /*-------------------------------- Functions --------------------------------*/
+function init(){
+clearInterval(timer)
+timer = null
+firstCard = null
+secondCard = null
+score = 0
 
+cardImages.sort(function (){
+    return Math.random()- 0.5
+})
 
-/*----------------------------- Event Listeners -----------------------------*/
+if(diffcultyElement.value==="easy"){
+    duration = 60
+    lives = 10
+}
+else if(diffcultyElement.value==="medium"){
+    duration = 45
+    lives = 5
+}
+else{
+    duration = 30
+    lives =3
+}
 
-
+for(let i=0;i<cardElement.length;i++){
+cardElement[i].textContent= "🏎️"
+cardElement[i].style.backgroundImage = "none"
+cardElement[i].style.visibility = "visible"
+}
+scoreElement.textContent ="🏆 Score: " + score + " / 6"
+durationElement.textContent = "⌛ Duration: " + duration
+livesElement.textContent = "⛽ Lives: " + lives
+messageElement.textContent = "Press Start 🏁"
+}
